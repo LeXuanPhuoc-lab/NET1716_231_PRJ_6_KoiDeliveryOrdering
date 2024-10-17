af5074c25f60bbe5ab7c4e4825346306e838ee79
Thiện Nguyễn <thiennqse171671@fpt.edu.vn>
2024-10-17 09:57:02 +0700

    [FIX] Fixx insert Truck

---
+++ KoiDeliveryOrdering.API/Payloads/Requests/TruckRequest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Logging.Abstractions;
+
+namespace KoiDeliveryOrdering.API.Payloads.Requests
+{
+    public class TruckRequest
+    {
+    }
+
+    public class CreateTruckRequest
+    {
+        public string TruckLicensePlate { get; set; } = null!;
+        public string Model { get; set; } = null!;
+        public int Capacity { get; set; }
+        public bool IsActive { get; set; } 
+        public int GarageId { get; set; }
+        public DateTime LastMaintenanceDate { get; set; } 
+    }
+}
